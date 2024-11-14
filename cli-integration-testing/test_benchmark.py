e4c6cd4d19f31961ca6047da20c8ded8c2fe840e
Jackson Boey <jackson_boey@imda.gov.sg>
2024-11-14 16:55:21 +0800

    Update test_benchmark and test_red_teaming tc

--- cli-integration-testing/test_benchmark.py
+++ cli-integration-testing/test_benchmark.py
@@ -1,20 +1,23 @@
-import random
-
-import pytest
 import subprocess
+import time
+
 from dotenv import load_dotenv
 import os
 
+import random
+
 load_dotenv()  # Load environment variables from .env file
 
-AZURE_OPENAI_URI = os.getenv('URI')
-AZURE_OPENAI_TOKEN = os.getenv('TOKEN')
+AZURE_OPENAI_URI = os.getenv('AZURE_OPENAI_URI')
+AZURE_OPENAI_TOKEN = os.getenv('AZURE_OPENAI_TOKEN')
+# CLI_DIR = '/Users/jacksonboey/PycharmProjects/moonshot'
+# CLI_DIR = os.getenv('CLI_DIR')
 CLI_DIR = '/Users/jacksonboey/PycharmProjects/moonshot'
 def test_cli_benchmark():
     command = (
-        'cd .. &&'
-        'source venv/bin/activate &&'
-        'cd moonshot &&'
+        # 'cd .. &&'
+        # 'source venv/bin/activate &&'
+        # 'cd moonshot &&'
         'python3 -m moonshot cli interactive'
     )
 
@@ -66,3 +69,152 @@ def test_cli_benchmark():
     last_line = output_lines[-16]
     print('=========================Output Last Line:', last_line)
     assert last_line.replace(" ", "") == "CookbookResult"
+
+def test_cli_add_cookbook():
+    command = (
+        'cd .. &&'
+        'source venv/bin/activate &&'
+        'cd moonshot &&'
+        'python3 -m moonshot cli interactive'
+    )
+
+    process = subprocess.Popen(
+        command,
+        shell=True,  # Allows for complex shell commands
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        text=True,
+        cwd=str(CLI_DIR),
+        # cwd="/Users/jacksonboey/PycharmProjects/moonshot",
+        #/home/runner/work/moonshot-data/moonshot-data for moonshot data repo
+        #/home/runner/work/moonshot/moonshot-data for moonshot repo
+    )
+    print('Path:', str(CLI_DIR))
+    # Ensure process.stdin is not None
+    if process.stdin is None:
+        raise RuntimeError("Failed to create stdin for the subprocess")
+
+    timestamp = time.time()  # Get the current timestamp in seconds
+    timestamp_int = str(int(timestamp))  # Remove the decimal part by converting to an integer
+    command = ('add_cookbook \'My new cookbook '+timestamp_int+'\' \'I am cookbook description\' "[\'analogical-similarity\','
+               '\'auto-categorisation\']"\n')
+    print('Command:', command)
+    # Example command to send to the process
+    process.stdin.write(command)
+    process.stdin.flush()
+
+    # Capture the output and errors
+    stdout, stderr = process.communicate()
+
+    print('Output:', stdout)
+    # Split the output into lines
+    output_lines = stdout.splitlines()
+
+    # Get the last line of the output
+    last_line = output_lines[11]
+    print('=========================Output Last Line:', last_line)
+    assert last_line.replace(" ", "") == "[add_cookbook]:Cookbook(my-new-cookbook-"+timestamp_int+")created."
+
+def test_cli_add_recipe():
+    command = (
+        'cd .. &&'
+        'source venv/bin/activate &&'
+        'cd moonshot &&'
+        'python3 -m moonshot cli interactive'
+    )
+
+    process = subprocess.Popen(
+        command,
+        shell=True,  # Allows for complex shell commands
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        text=True,
+        cwd=str(CLI_DIR),
+        # cwd="/Users/jacksonboey/PycharmProjects/moonshot",
+        #/home/runner/work/moonshot-data/moonshot-data for moonshot data repo
+        #/home/runner/work/moonshot/moonshot-data for moonshot repo
+    )
+    print('Path:', str(CLI_DIR))
+    # Ensure process.stdin is not None
+    if process.stdin is None:
+        raise RuntimeError("Failed to create stdin for the subprocess")
+
+    timestamp = time.time()  # Get the current timestamp in seconds
+    timestamp_int = str(int(timestamp))  # Remove the decimal part by converting to an integer
+    command = ('add_recipe \'My new recipe '+timestamp_int+'\' \'I am recipe description\' "[\'category1\',\'category2\']" "[\'bbq-lite-age-ambiguous\']" "[\'bertscore\',\'bleuscore\']" -p "[\'analogical-similarity\',\'mmlu\']" -t "[\'tag1\',\'tag2\']" -g "{\'A\':[80,100],\'B\':[60,79],\'C\':[40,59],\'D\':[20,39],\'E\':[0,19]}"\n')
+    print('Command:', command)
+    # Example command to send to the process
+    process.stdin.write(command)
+    process.stdin.flush()
+
+    # Capture the output and errors
+    stdout, stderr = process.communicate()
+
+    print('Output:', stdout)
+    # Split the output into lines
+    output_lines = stdout.splitlines()
+
+    # Get the last line of the output
+    last_line = output_lines[11]
+    print('=========================Output Last Line:', last_line)
+    assert last_line.replace(" ", "") == "[add_recipe]:Recipe(my-new-recipe-"+timestamp_int+")created."
+
+def test_cli_delete_recipe():
+    command = (
+        'cd .. &&'
+        'source venv/bin/activate &&'
+        'cd moonshot &&'
+        'python3 -m moonshot cli interactive'
+    )
+
+    process = subprocess.Popen(
+        command,
+        shell=True,  # Allows for complex shell commands
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        text=True,
+        cwd=str(CLI_DIR),
+        # cwd="/Users/jacksonboey/PycharmProjects/moonshot",
+        #/home/runner/work/moonshot-data/moonshot-data for moonshot data repo
+        #/home/runner/work/moonshot/moonshot-data for moonshot repo
+    )
+    print('Path:', str(CLI_DIR))
+    # Ensure process.stdin is not None
+    if process.stdin is None:
+        raise RuntimeError("Failed to create stdin for the subprocess")
+
+    timestamp = time.time()  # Get the current timestamp in seconds
+    timestamp_int = str(int(timestamp))  # Remove the decimal part by converting to an integer
+    command = ('add_recipe \'My new recipe '+timestamp_int+'\' \'I am recipe description\' "[\'category1\',\'category2\']" "[\'bbq-lite-age-ambiguous\']" "[\'bertscore\',\'bleuscore\']" -p "[\'analogical-similarity\',\'mmlu\']" -t "[\'tag1\',\'tag2\']" -g "{\'A\':[80,100],\'B\':[60,79],\'C\':[40,59],\'D\':[20,39],\'E\':[0,19]}"\n')
+    print('Command:', command)
+    # Example command to send to the process
+    process.stdin.write(command)
+    process.stdin.flush()
+
+    command = ('delete_recipe my-new-recipe-'+timestamp_int+'\n')
+    print('Command:', command)
+    # Example command to send to the process
+    process.stdin.write(command)
+    process.stdin.flush()
+
+    command = ('y\n')
+    print('Command:', command)
+    # Example command to send to the process
+    process.stdin.write(command)
+    process.stdin.flush()
+
+
+    # Capture the output and errors
+    stdout, stderr = process.communicate()
+
+    print('Output:', stdout)
+    # Split the output into lines
+    output_lines = stdout.splitlines()
+
+    # Get the last line of the output
+    last_line = output_lines[12]
+    print('=========================Output Last Line:', last_line)
+    assert last_line.replace(" ", "") == "Areyousureyouwanttodeletetherecipe(y/N)?[delete_recipe]:Recipedeleted."
--- cli-integration-testing/test_redteam.py
+++ cli-integration-testing/test_redteam.py
@@ -1,28 +1,75 @@
+import random
+
+import pytest
 import subprocess
+from dotenv import load_dotenv
+import os
 
-def test_redteam():
+import random
 
-    # session_command = subprocess.Popen(["echo","use_session 'my-runner'" ], stdout=subprocess.PIPE, text=True)
-    
-    # Setup Moonshot CLI Subprocess
-    run_cli_process = subprocess.Popen(["python3", "-m", "moonshot", "cli", "interactive"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, text=True, cwd="/Users/benedict/Documents/GitHub/test")
-    
-    # Send CLI Commands
-    run_cli_process.stdin.write("use_session 'my-runner'" + "\n")
-    run_cli_process.stdin.flush()
+load_dotenv()  # Load environment variables from .env file
 
-    # output, errors = run_cli_process.communicate(input=["use_session 'my_runner'","run_attack_module sample_attack_module \"this is my prompt\" -s \"test system prompt\" -m bleuscore"])
-    run_cli_process.stdin.write('run_attack_module "this is my prompt" -s "test system prompt" -m bleuscore' + "\n") # Opens IO stream, write input, provide output and close IO stream
-    run_cli_process.stdin.flush()
-    run_cli_process.stdin.close()
+AZURE_OPENAI_URI = os.getenv('AZURE_OPENAI_URI')
+AZURE_OPENAI_TOKEN = os.getenv('AZURE_OPENAI_TOKEN')
+# CLI_DIR = '/Users/jacksonboey/PycharmProjects/moonshot'
+CLI_DIR = os.getenv('CLI_DIR')
+def test_cli_red_teaming():
+    command = (
+        # 'cd .. &&'
+        # 'source venv/bin/activate &&'
+        # 'cd moonshot &&'
+        'python3 -m moonshot cli interactive'
+    )
+    process = subprocess.Popen(
+        command,
+        shell=True,  # Allows for complex shell commands
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        text=True,
+        cwd=str(CLI_DIR),
+    )
 
-    output = run_cli_process.stdout.read()
-    
-    # run_cli_process.stdin.close()
-    # output = run_cli_process.stdout.read()
+    # Ensure process.stdin is not None
+    if process.stdin is None:
+        raise RuntimeError("Failed to create stdin for the subprocess")
 
-    # Print CLI Output
-    print(output)
-    # print(errors)
+    # # Example command to send to the process
+    # process.stdin.write('list_cookbooks\n')
+    # process.stdin.flush()
 
-    assert output == "Test 1000"
+    # Generate a random number between 0 and 999,999,999 (inclusive)
+    random_number = int(random.random() * 1000000000)
+    nameOfRunnerFileName = "my-red-teaming-runner-" + str(random_number)
+
+    file_path = str(CLI_DIR)+"/moonshot-data/generated-outputs/runners/"+nameOfRunnerFileName+".json"
+
+    # # Example command to send to the process
+    # process.stdin.write('use_session "my-runner"\n')
+    # process.stdin.flush()
+
+    command = 'new_session '+nameOfRunnerFileName+' -e "[\'ollama-llama3\']" -c add_previous_prompt -p mmlu \n'
+    # Example command to send to the process
+    process.stdin.write(command)
+    process.stdin.flush()
+
+    # Example command to send to the process
+    process.stdin.write('run_attack_module charswap_attack "this is my prompt"\n')
+    process.stdin.flush()
+
+    # # Capture the output and errors
+    # stdout, stderr = process.communicate()
+
+    # Capture the output and errors
+    stdout, stderr = process.communicate()
+
+    print('Output:', stdout)
+
+    if os.path.exists(file_path):
+        print(f"File exists: {file_path}")
+        assert True
+
+
+    else:
+        print(f"File does not exist: {file_path}")
+        pytest.fail()
