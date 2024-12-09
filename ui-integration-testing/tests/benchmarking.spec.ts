80d39c1219884663ad626b5e12439eb385d1bdf3
Jackson Boey <jackson_boey@imda.gov.sg>
2024-12-09 17:45:23 +0800

    resolve issue with dotenv

--- ui-integration-testing/tests/benchmarking.spec.ts
+++ ui-integration-testing/tests/benchmarking.spec.ts
@@ -2,7 +2,6 @@ import {test} from '@playwright/test';
 import {expect} from "@playwright/test";
 // import {create_endpoint_steps} from './endpoint.spec';
 import dotenv from 'dotenv';
-dotenv.config({path: path.resolve(__dirname, '.env')});
 import path from 'path';
 import fs from "fs/promises";
 // Read from ".env" file.
