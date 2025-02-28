cbdb5f1fe0d99b708f18c6a479ea8dca9fbb4a3e
Jackson Boey <jackson_boey@imda.gov.sg>
2025-02-28 16:43:34 +0800

    Update assertion and add parameter testing cases

--- moonshot-v1-cli-integration/test_benchmark.py
+++ moonshot-v1-cli-integration/test_benchmark.py
@@ -1,33 +1,194 @@
 import subprocess
-
 import pytest
 from dotenv import load_dotenv
 import os
 import random
+from util import parametrize, INPUT_PARAMS
 
 load_dotenv()  # Load environment variables from .env file
 
 OPENAI_TOKEN = os.getenv('OPENAI_TOKEN')
 MOON_V1_CLI_DIR = os.getenv('MOON_V1_CLI_DIR')
 
-def assert_run_outcome(output_lines,test_run_name):
-    # Get the last line of the output
-    line_3 = output_lines[-3]
-    line_3_expected = "successfully created with run_id:"
-    line_2_1 = output_lines[-2] + output_lines[-1]
-    line_2_1_expected = test_run_name
-    print('=========================Output Last Line:', line_3)
-    print('=========================Output Last Line:', line_2_1)
-    assert line_3.replace(" ", "") == line_3_expected.replace(" ", "")
-    assert line_2_1.replace(" ", "") == line_2_1_expected.replace(" ", "")
 
-@pytest.mark.skip(reason="This test is skipped for now")
+EXPECTED_OUTCOME = [
+    ("No valid file found for 1 in ", 14),  # Expected result for 1
+    ("No valid file found for 1.1 in", 14),  # Expected result for 1.1
+    ("", 0),  # Expected result for -1
+    ("No valid file found for 0 in", 14),  # Expected result for 0
+    ("No valid file found for @1 in", 14),  # Expected result for "@1"
+    ("No valid file found for test in", 14)  # Expected result for "test"
+]
+@parametrize("input_params, expected", zip(INPUT_PARAMS, EXPECTED_OUTCOME))
+def test_cli_run_benchmarking_params_testing_dataset_module(input_params,expected):
+    expectedMsg, expected_value = expected  # Extract the second value
+    # Generate a random number between 0 and 999,999,999 (inclusive)
+    random_number = int(random.random() * 1000000000)
+    dataset_module = str(input_params)
+    connector_name = "my-gpt4o"
+    nameOfRunnerName = "my-benchmarking-" + connector_name + "-" + dataset_module + "-" + str(random_number)
+    metric_module = "refusal_adapter"
+
+    commands = [
+        "export OPENAI_API_KEY=" + OPENAI_TOKEN,
+        "poetry run moonshot benchmark " + nameOfRunnerName + " " + dataset_module + " " + metric_module + " " + connector_name + ""
+    ]
+    # Join commands with '&&' to ensure the next runs only if the previous succeeds
+    full_command = "&&".join(commands)
+    print(f"Running combined command: {full_command}")
+
+    process = subprocess.Popen(
+        full_command,
+        shell=True,  # Allows for complex shell commands
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        text=True,
+        cwd=str(MOON_V1_CLI_DIR),
+    )
+    print('Path:', str(MOON_V1_CLI_DIR))
+    # Ensure process.stdin is not None
+    if process.stdin is None:
+        raise RuntimeError("Failed to create stdin for the subprocess")
+
+    # Capture the output and errors
+    stdout, stderr = process.communicate()
+
+    print('Output:', stdout)
+    # Split the output into lines
+    output_lines = [line.replace(" ", "") for line in stdout.splitlines() if line.strip()]
+    # Assert Outcome
+    if input_params == -1:
+        # Assert that the subprocess failed
+        assert process.returncode != 0  # Ensure it exits with an error
+        # Assert that the error contains Pydantic's validation message
+        assert "Error: No such option: -1\n"
+    else:
+        assert expectedMsg.replace(" ", "") in output_lines
+
+CONNECTOR_EXPECTED_OUTCOME = [
+    ("ERROR    [TaskManager] Error loading the task_manager.py:483", 6),  # Expected result for 1
+    ("ERROR    [TaskManager] Error loading the task_manager.py:483", 6),  # Expected result for 1.1
+    ("", 0),  # Expected result for -1
+    ("ERROR    [TaskManager] Error loading the task_manager.py:483", 6),  # Expected result for 0
+    ("ERROR    [TaskManager] Error loading the task_manager.py:483", 6),  # Expected result for "@1"
+    ("ERROR    [TaskManager] Error loading the task_manager.py:483", 6)  # Expected result for "test"
+]
+@parametrize("input_params, expected", zip(INPUT_PARAMS, CONNECTOR_EXPECTED_OUTCOME))
+def test_cli_run_benchmarking_params_testing_connector_name(input_params,expected):
+    expectedMsg, expected_value = expected  # Extract the second value
+    # Generate a random number between 0 and 999,999,999 (inclusive)
+    random_number = int(random.random() * 1000000000)
+    dataset_module = "mmlu-mini"
+    connector_name = str(input_params)
+    nameOfRunnerName = "my-benchmarking-" + connector_name + "-" + dataset_module + "-" + str(random_number)
+    metric_module = "refusal_adapter"
+
+    commands = [
+        "export OPENAI_API_KEY=" + OPENAI_TOKEN,
+        "poetry run moonshot benchmark " + nameOfRunnerName + " " + dataset_module + " " + metric_module + " " + connector_name + ""
+    ]
+    # Join commands with '&&' to ensure the next runs only if the previous succeeds
+    full_command = "&&".join(commands)
+    print(f"Running combined command: {full_command}")
+
+    process = subprocess.Popen(
+        full_command,
+        shell=True,  # Allows for complex shell commands
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        text=True,
+        cwd=str(MOON_V1_CLI_DIR),
+    )
+    print('Path:', str(MOON_V1_CLI_DIR))
+    # Ensure process.stdin is not None
+    if process.stdin is None:
+        raise RuntimeError("Failed to create stdin for the subprocess")
+
+    # Capture the output and errors
+    stdout, stderr = process.communicate()
+
+    print('Output:', stdout)
+    # Split the output into lines
+    output_lines = [line.replace(" ", "") for line in stdout.splitlines() if line.strip()]
+    # Assert Outcome
+    if input_params == -1:
+        # Assert that the subprocess failed
+        assert process.returncode != 0  # Ensure it exits with an error
+        # Assert that the error contains Pydantic's validation message
+        assert "Error: No such option: -1\n"
+    else:
+        assert expectedMsg.replace(" ", "") in output_lines
+
+METRIC_MODULE_EXPECTED_OUTCOME = [
+    ("Error loading metric", 6),  # Expected result for 1
+    ("Error loading metric", 6),  # Expected result for 1.1
+    ("", 0),  # Expected result for -1
+    ("Error loading metric", 6),  # Expected result for 0
+    ("Error loading metric", 6),  # Expected result for "@1"
+    ("Error loading metric", 6)  # Expected result for "test"
+]
+@parametrize("input_params, expected", zip(INPUT_PARAMS, METRIC_MODULE_EXPECTED_OUTCOME))
+def test_cli_run_benchmarking_params_testing_metric_module(input_params,expected):
+    expectedMsg, expected_value = expected  # Extract the second value
+    # Generate a random number between 0 and 999,999,999 (inclusive)
+    random_number = int(random.random() * 1000000000)
+    dataset_module = "mmlu-mini"
+    connector_name = "my-gpt4o"
+    nameOfRunnerName = "my-benchmarking-" + connector_name + "-" + dataset_module + "-" + str(random_number)
+    metric_module = str(input_params)
+
+    commands = [
+        "export OPENAI_API_KEY=" + OPENAI_TOKEN,
+        "poetry run moonshot benchmark " + nameOfRunnerName + " " + dataset_module + " " + metric_module + " " + connector_name + ""
+    ]
+    # Join commands with '&&' to ensure the next runs only if the previous succeeds
+    full_command = "&&".join(commands)
+    print(f"Running combined command: {full_command}")
+
+    process = subprocess.Popen(
+        full_command,
+        shell=True,  # Allows for complex shell commands
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        text=True,
+        cwd=str(MOON_V1_CLI_DIR),
+    )
+    print('Path:', str(MOON_V1_CLI_DIR))
+    # Ensure process.stdin is not None
+    if process.stdin is None:
+        raise RuntimeError("Failed to create stdin for the subprocess")
+
+    # Capture the output and errors
+    stdout, stderr = process.communicate()
+
+    print('Output:', stdout)
+    # Split the output into lines
+    output_lines = [line.replace(" ", "") for line in stdout.splitlines() if line.strip()]
+    # Assert Outcome
+    if input_params == -1:
+        # Assert that the subprocess failed
+        assert process.returncode != 0  # Ensure it exits with an error
+        # Assert that the error contains Pydantic's validation message
+        assert "Error: No such option: -1\n"
+    else:
+        assert expectedMsg.replace(" ", "") in output_lines
+def assert_run_outcome(output_lines):
+    output_lines = [line.replace(" ", "") for line in output_lines if line.strip()]
+
+    assert "File written".replace(" ", "") in output_lines
+    assert "successfully at:".replace(" ", "") in output_lines
+    assert "data/results/my-benchm".replace(" ", "") in output_lines
+    assert "successfully created with run_id:".replace(" ", "") in output_lines
+
 def test_cli_run_benchmarking_refusal_adapter_mmlu_mini():
 
     # Generate a random number between 0 and 999,999,999 (inclusive)
     random_number = int(random.random() * 1000000000)
     dataset_module = "mmlu-mini"
-    connector_name = "my-gpt-o1"
+    connector_name = "my-gpt4o"
     nameOfRunnerName = "my-benchmarking-" + connector_name + "-" + dataset_module + "-" + str(random_number)
     metric_module = "refusal_adapter"
 
@@ -59,28 +220,15 @@ def test_cli_run_benchmarking_refusal_adapter_mmlu_mini():
     print('Output:', stdout)
     # Split the output into lines
     output_lines = stdout.splitlines()
-    #Assert Results
-    # Get the last line of the output
-    line_13 = output_lines[-13]
-    line_13_expected = "File written"
-    line_12 = output_lines[-12]
-    line_12_expected = "successfully at:"
-    line_11 = output_lines[-11]
-    line_11_expected = "data/results/my-benchm"
-    print('=========================Output Last Line:', line_13)
-    print('=========================Output Last Line:', line_12)
-    print('=========================Output Last Line:', line_11)
-    assert line_13.replace(" ", "") == line_13_expected.replace(" ", "")
-    assert line_12.replace(" ", "") == line_12_expected.replace(" ", "")
-    assert line_11.replace(" ", "") == line_11_expected.replace(" ", "")
-    assert_run_outcome(output_lines,nameOfRunnerName)
+    # Assert Results
+    assert_run_outcome(output_lines)
 @pytest.mark.skip(reason="This test is skipped for now")
 def test_cli_run_benchmarking_refusal_adapter_prompt_injection_obfuscation():
 
     # Generate a random number between 0 and 999,999,999 (inclusive)
     random_number = int(random.random() * 1000000000)
     dataset_module = "prompt_injection_obfuscation"
-    connector_name = "my-gpt-o1"
+    connector_name = "my-gpt4o"
     nameOfRunnerName = "my-benchmarking-"+connector_name+"-"+dataset_module+"-" + str(random_number)
     metric_module = "refusal_adapter"
 
@@ -114,14 +262,14 @@ def test_cli_run_benchmarking_refusal_adapter_prompt_injection_obfuscation():
     output_lines = stdout.splitlines()
 
     # Assert Results
-    assert_run_outcome(output_lines, nameOfRunnerName)
+    assert_run_outcome(output_lines)
 @pytest.mark.skip(reason="This test is skipped for now")
 def test_cli_run_benchmarking_refusal_adapter_prompt_injection_payload_splitting():
 
     # Generate a random number between 0 and 999,999,999 (inclusive)
     random_number = int(random.random() * 1000000000)
     dataset_module = "prompt_injection_payload_splitting"
-    connector_name = "my-gpt-o1"
+    connector_name = "my-gpt4o"
     nameOfRunnerName = "my-benchmarking-" + connector_name + "-" + dataset_module + "-" + str(random_number)
     metric_module = "refusal_adapter"
 
@@ -155,22 +303,14 @@ def test_cli_run_benchmarking_refusal_adapter_prompt_injection_payload_splitting
     output_lines = stdout.splitlines()
 
     # Assert Results
-    # Get the last line of the output
-    line_4 = output_lines[-4]
-    line_4_expected = "successfully created with run_id:"
-    line_3_2_1 = output_lines[-3]+output_lines[-2] + output_lines[-1]
-    line_3_2_1_expected = nameOfRunnerName
-    print('=========================Output Last Line:', line_4)
-    print('=========================Output Last Line:', line_3_2_1)
-    assert line_4.replace(" ", "") == line_4_expected.replace(" ", "")
-    assert line_3_2_1.replace(" ", "") == line_3_2_1_expected.replace(" ", "")
+    assert_run_outcome(output_lines)
 @pytest.mark.skip(reason="This test is skipped for now")
 def test_cli_run_benchmarking_refusal_adapter_prompt_injection_role_playing():
 
     # Generate a random number between 0 and 999,999,999 (inclusive)
     random_number = int(random.random() * 1000000000)
     dataset_module = "prompt_injection_role_playing"
-    connector_name = "my-gpt-o1"
+    connector_name = "my-gpt4o"
     nameOfRunnerName = "my-benchmarking-" + connector_name + "-" + dataset_module + "-" + str(random_number)
     metric_module = "refusal_adapter"
 
@@ -204,14 +344,14 @@ def test_cli_run_benchmarking_refusal_adapter_prompt_injection_role_playing():
     output_lines = stdout.splitlines()
 
     # Assert Results
-    assert_run_outcome(output_lines, nameOfRunnerName)
+    assert_run_outcome(output_lines)
 @pytest.mark.skip(reason="This test is skipped for now")
 def test_cli_run_benchmarking_refusal_adapter_sensitive_data_disclosure_general():
 
     # Generate a random number between 0 and 999,999,999 (inclusive)
     random_number = int(random.random() * 1000000000)
     dataset_module = "sensitive_data_disclosure_general"
-    connector_name = "my-gpt-o1"
+    connector_name = "my-gpt4o"
     nameOfRunnerName = "my-benchmarking-" + connector_name + "-" + dataset_module + "-" + str(random_number)
     metric_module = "refusal_adapter"
 
@@ -245,4 +385,4 @@ def test_cli_run_benchmarking_refusal_adapter_sensitive_data_disclosure_general(
     output_lines = stdout.splitlines()
 
     # Assert Results
-    assert_run_outcome(output_lines, nameOfRunnerName)
+    assert_run_outcome(output_lines)
--- moonshot-v1-cli-integration/test_redteam.py
+++ moonshot-v1-cli-integration/test_redteam.py
@@ -11,33 +11,27 @@ load_dotenv()  # Load environment variables from .env file
 OPENAI_TOKEN = os.getenv('OPENAI_TOKEN')
 MOON_V1_CLI_DIR = os.getenv('MOON_V1_CLI_DIR')
 
-def assert_run_outcome(output_lines,test_run_name):
-    # Get the last line of the output
-    line_3 = output_lines[-3]
-    line_3_expected = "successfully created with"
-    line_2 = output_lines[-2]
-    line_2_expected = "run_id:"
-    line_1 = output_lines[-1]
-    line_1_expected = test_run_name
-    print('=========================Output Last Line:', line_3)
-    print('=========================Output Last Line:', line_2)
-    print('=========================Output Last Line:', line_1)
-    assert line_3.replace(" ", "") == line_3_expected.replace(" ", "")
-    assert line_2.replace(" ", "") == line_2_expected.replace(" ", "")
-    assert line_1.replace(" ", "") == line_1_expected.replace(" ", "")
+
+def assert_run_outcome(output_lines):
+    output_lines = [line.replace(" ", "") for line in output_lines if line.strip()]
+    assert "File written".replace(" ", "") in output_lines
+    assert "successfully at:".replace(" ", "") in output_lines
+    assert "data/results/test_run_".replace(" ", "") in output_lines
+    assert "successfully created with".replace(" ", "") in output_lines
+    assert "run_id:".replace(" ", "") in output_lines
 
 EXPECTED_OUTCOME = [
-    "successfully created with",      # Expected result for 1
-    "successfully created with",      # Expected result for 1.1
-    "successfully created with",      # Expected result for -1
-    "successfully created with",      # Expected result for 0
-    "successfully created with",     # Expected result for "@1"
-    "successfully created with",     # Expected result for "test"
+    ("Error loading attack", 25),  # Expected result for 1
+    ("Error loading attack", 14),  # Expected result for 1.1
+    ("", 0),  # Expected result for -1
+    ("Error loading attack", 25),  # Expected result for 0
+    ("Error loading attack", 25),  # Expected result for "@1"
+    ("No valid file found for test in", 14)  # Expected result for "test"
 ]
-@pytest.mark.skip(reason="This test is skipped for now")
-@parametrize("input_params, expected", zip(INPUT_PARAMS, EXPECTED_OUTCOME))
-def test_cli_run_create_scan_test_params_testing(input_params,expected):
 
+@parametrize("input_params, expected", zip(INPUT_PARAMS, EXPECTED_OUTCOME))
+def test_cli_run_create_scan_test_params_testing_attack_module(input_params,expected):
+    expectedMsg, expected_value = expected  # Extract the second value
     # Generate a random number between 0 and 999,999,999 (inclusive)
     random_number = int(random.random() * 1000000000)
     attack_module = str(input_params)
@@ -72,17 +66,31 @@ def test_cli_run_create_scan_test_params_testing(input_params,expected):
     print('Output:', stdout)
     # Split the output into lines
     output_lines = stdout.splitlines()
-    #Assert Outcome
-    assert output_lines[-4].replace(" ", "") == expected.replace(" ", "")
+    # Assert Outcome
+    if input_params == -1:
+        # Assert that the subprocess failed
+        assert process.returncode != 0  # Ensure it exits with an error
+        # Assert that the error contains Pydantic's validation message
+        assert "Error: No such option: -1\n"
+    else:
+        assert output_lines[-expected_value].replace(" ", "") == expectedMsg.replace(" ", "")
 
-@pytest.mark.skip(reason="This test is skipped for now")
-def test_cli_run_redteaming_hallucination_refusal_adapter():
-
+METRIC_PARAM_EXPECTED_OUTCOME = [
+    ("Error loading metric", 43),  # Expected result for 1
+    ("Error loading metric", 67),  # Expected result for 1.1
+    ("", 0),  # Expected result for -1
+    ("Error loading metric", 43),  # Expected result for 0
+    ("Error loading metric", 43),  # Expected result for "@1"
+    ("No valid file found for test in", 14)  # Expected result for "test"
+]
+@parametrize("input_params, expected", zip(INPUT_PARAMS, METRIC_PARAM_EXPECTED_OUTCOME))
+def test_cli_run_create_scan_test_params_testing_metric_module(input_params,expected):
+    expectedMsg, expected_value = expected  # Extract the second value
     # Generate a random number between 0 and 999,999,999 (inclusive)
     random_number = int(random.random() * 1000000000)
     attack_module = "hallucination"
-    metric_module = "refusal_adapter"
-    connector_name = "my-gpt-o1"
+    metric_module = str(input_params)
+    connector_name = "my-gpt4o"
     nameOfRunnerName = "test_run_" + connector_name + "_" + attack_module + "_" + str(random_number)
     commands = [
         "export OPENAI_API_KEY="+OPENAI_TOKEN,
@@ -113,27 +121,118 @@ def test_cli_run_redteaming_hallucination_refusal_adapter():
     # Split the output into lines
     output_lines = stdout.splitlines()
     #Assert Outcome
-    # Get the last line of the output
-    line_14 = output_lines[-14]
-    line_14_expected = "File written"
-    line_13 = output_lines[-13]
-    line_13_expected = "successfully at:"
-    line_12 = output_lines[-12]
-    line_12_expected = "data/results/test_run_"
-    print('=========================Output Last Line:', line_14)
-    print('=========================Output Last Line:', line_13)
-    print('=========================Output Last Line:', line_12)
-    assert line_14.replace(" ", "") == line_14_expected.replace(" ", "")
-    assert line_13.replace(" ", "") == line_13_expected.replace(" ", "")
-    assert line_12.replace(" ", "") == line_12_expected.replace(" ", "")
-    assert_run_outcome(output_lines,nameOfRunnerName)
+    # Assert Outcome
+    if input_params == -1:
+        # Assert that the subprocess failed
+        assert process.returncode != 0  # Ensure it exits with an error
+        # Assert that the error contains Pydantic's validation message
+        assert "Error: No such option: -1\n"
+    else:
+        assert output_lines[-expected_value].replace(" ", "") == expectedMsg.replace(" ", "")
+
+
+CONNECTOR_PARAM_EXPECTED_OUTCOME = [
+    ("r] Failed", 14),  # Expected result for 1
+    ("r] Failed", 14),  # Expected result for 1.1
+    ("", 0),  # Expected result for -1
+    ("r] Failed", 14),  # Expected result for 0
+    ("r] Failed", 14),  # Expected result for "@1"
+    ("r] Failed", 14)  # Expected result for "test"
+]
+@parametrize("input_params, expected", zip(INPUT_PARAMS, CONNECTOR_PARAM_EXPECTED_OUTCOME))
+def test_cli_run_create_scan_test_params_testing_connector_name(input_params,expected):
+    expectedMsg, expected_value = expected  # Extract the second value
+    # Generate a random number between 0 and 999,999,999 (inclusive)
+    random_number = int(random.random() * 1000000000)
+    attack_module = "hallucination"
+    metric_module = "refusal_adapter"
+    connector_name = str(input_params)
+    nameOfRunnerName = "test_run_" + connector_name + "_" + attack_module + "_" + str(random_number)
+    commands = [
+        "export OPENAI_API_KEY="+OPENAI_TOKEN,
+        "poetry run moonshot scan " + nameOfRunnerName + " "+attack_module+" "+metric_module+" "+connector_name+""
+    ]
+    # Join commands with '&&' to ensure the next runs only if the previous succeeds
+    full_command = "&&".join(commands)
+    print(f"Running combined command: {full_command}")
+
+    process = subprocess.Popen(
+        full_command,
+        shell=True,  # Allows for complex shell commands
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        text=True,
+        cwd=str(MOON_V1_CLI_DIR),
+    )
+    print('Path:', str(MOON_V1_CLI_DIR))
+    # Ensure process.stdin is not None
+    if process.stdin is None:
+        raise RuntimeError("Failed to create stdin for the subprocess")
+
+    # Capture the output and errors
+    stdout, stderr = process.communicate()
+
+    print('Output:', stdout)
+    # Split the output into lines
+    output_lines = stdout.splitlines()
+    #Assert Outcome
+    # Assert Outcome
+    if input_params == -1:
+        # Assert that the subprocess failed
+        assert process.returncode != 0  # Ensure it exits with an error
+        # Assert that the error contains Pydantic's validation message
+        assert "Error: No such option: -1\n"
+    else:
+        assert output_lines[-expected_value].replace(" ", "") == expectedMsg.replace(" ", "")
+
+
+
+def test_cli_run_redteaming_hallucination_refusal_adapter():
+
+    # Generate a random number between 0 and 999,999,999 (inclusive)
+    random_number = int(random.random() * 1000000000)
+    attack_module = "hallucination"
+    metric_module = "refusal_adapter"
+    connector_name = "my-gpt4o"
+    nameOfRunnerName = "test_run_" + connector_name + "_" + attack_module + "_" + str(random_number)
+    commands = [
+        "export OPENAI_API_KEY="+OPENAI_TOKEN,
+        "poetry run moonshot scan " + nameOfRunnerName + " "+attack_module+" "+metric_module+" "+connector_name+""
+    ]
+    # Join commands with '&&' to ensure the next runs only if the previous succeeds
+    full_command = "&&".join(commands)
+    print(f"Running combined command: {full_command}")
+
+    process = subprocess.Popen(
+        full_command,
+        shell=True,  # Allows for complex shell commands
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        text=True,
+        cwd=str(MOON_V1_CLI_DIR),
+    )
+    print('Path:', str(MOON_V1_CLI_DIR))
+    # Ensure process.stdin is not None
+    if process.stdin is None:
+        raise RuntimeError("Failed to create stdin for the subprocess")
+
+    # Capture the output and errors
+    stdout, stderr = process.communicate()
+
+    print('Output:', stdout)
+    # Split the output into lines
+    output_lines = stdout.splitlines()
+    #Assert Outcome
+    assert_run_outcome(output_lines)
 @pytest.mark.skip(reason="This test is skipped for now")
 def test_cli_run_redteaming_sensitive_data_disclosure_refusal_adapter():
 
     # Generate a random number between 0 and 999,999,999 (inclusive)
     random_number = int(random.random() * 1000000000)
     attack_module = "sensitive_data_disclosure"
-    connector_name = "my-gpt-o1"
+    connector_name = "my-gpt4o"
     nameOfRunnerName = "test_run_" + connector_name + "_" + attack_module + "_" + str(random_number)
     metric_module = "refusal_adapter"
     commands = [
@@ -165,14 +264,14 @@ def test_cli_run_redteaming_sensitive_data_disclosure_refusal_adapter():
     # Split the output into lines
     output_lines = stdout.splitlines()
     # Assert Outcome
-    assert_run_outcome(output_lines,nameOfRunnerName)
+    assert_run_outcome(output_lines)
 @pytest.mark.skip(reason="This test is skipped for now")
 def test_cli_run_redteaming_system_prompt_leakage_refusal_adapter():
 
     # Generate a random number between 0 and 999,999,999 (inclusive)
     random_number = int(random.random() * 1000000000)
     attack_module = "system_prompt_leakage"
-    connector_name = "my-gpt-o1"
+    connector_name = "my-gpt4o"
     nameOfRunnerName = "test_run_"+connector_name+"_"+attack_module+"_" + str(random_number)
     metric_module = "refusal_adapter"
     commands = [
@@ -204,18 +303,5 @@ def test_cli_run_redteaming_system_prompt_leakage_refusal_adapter():
     # Split the output into lines
     output_lines = stdout.splitlines()
 
-    #Assert Outcome
-    # Get the last line of the output
-    line_16 = output_lines[-16]
-    line_16_expected = "File written"
-    line_15 = output_lines[-15]
-    line_15_expected = "successfully at:"
-    line_14 = output_lines[-14]
-    line_14_expected = "data/results/test_run_"
-    print('=========================Output Last Line:', line_16)
-    print('=========================Output Last Line:', line_15)
-    print('=========================Output Last Line:', line_14)
-    assert line_16.replace(" ", "") == line_16_expected.replace(" ", "")
-    assert line_15.replace(" ", "") == line_15_expected.replace(" ", "")
-    assert line_14.replace(" ", "") == line_14_expected.replace(" ", "")
-    assert_run_outcome(output_lines, nameOfRunnerName)
+    # Assert Outcome
+    assert_run_outcome(output_lines)
